b89aa411f4c39aa1f04516d86b7d250fa99e224e
ugurdnis <ugurdnis@gmail.com>
2020-12-28 16:34:09 +0300

    Create Player.cs

---
+++ TabooGame/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace TabooGame.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string LobbyName { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public Player(string name, string lobbyName)
+        {
+            Name = name;
+            LobbyName = lobbyName;
+        }
+    }
+}

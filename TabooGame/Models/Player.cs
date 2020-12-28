e1ac067f1a5d042832aeafd419261952c631fa73
ugurdnis <ugurdnis@gmail.com>
2020-12-28 17:15:34 +0300

    Add ID property

--- TabooGame/Models/Player.cs
+++ TabooGame/Models/Player.cs
@@ -2,6 +2,7 @@
 {
     public class Player
     {
+        public string ID { get; set; }
         public string Name { get; set; }
         public string LobbyName { get; set; }
         public bool IsAdmin { get; set; }

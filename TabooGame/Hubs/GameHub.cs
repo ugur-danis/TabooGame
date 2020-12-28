8e6ff9c1a325f2f744a318c488524e907e882a89
ugurdnis <ugurdnis@gmail.com>
2020-12-28 17:16:04 +0300

    Create GameHub.cs

---
+++ TabooGame/Hubs/GameHub.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using TabooGame.Data;
+using TabooGame.Models;
+
+namespace TabooGame.Hubs
+{
+    public class GameHub : Hub
+    {
+        public const string url = "/gameHub";
+
+        public void PlayerLogin(Player player)
+        {
+            player.ID = Context.ConnectionId;
+            GameDatabase.players.Add(player);
+        }
+
+        public async Task CreateLobby(Player player)
+        {
+            await Groups.AddToGroupAsync(player.ID, player.LobbyName);
+        }
+
+        public async Task JoinLobby(Player player)
+        {
+            await Groups.AddToGroupAsync(player.ID, player.LobbyName);
+            await Clients.OthersInGroup(player.LobbyName).SendAsync($"{player.Name} is join the lobby.");
+        }
+    }
+}

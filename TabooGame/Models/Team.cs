191e10ad69ba4429cf80b548d28632d270e2f246
ugurdnis <ugurdnis@gmail.com>
2020-12-31 01:00:39 +0300

    Create Team Model

---
+++ TabooGame/Models/Team.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TabooGame.Models
+{
+    public class Team
+    {
+        public int Score { get; set; }
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}

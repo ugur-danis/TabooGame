089062d7412c1dd592e95d64396833eddb1468c8
ugurdnis <ugurdnis@gmail.com>
2020-12-31 01:01:09 +0300

    Create WordCardDataBase.cs

---
+++ TabooGame/Data/WordCardDataBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using TabooGame.Models;
+
+namespace TabooGame.Data
+{
+    public static class WordCardDataBase
+    {
+        public static List<WordCard> WordCards = new List<WordCard>
+        {
+            new WordCard
+            {
+                ID = 0,
+                Word = "Salata",
+                ForbiddenWords = new List<string>
+                {
+                    "Sebze","Meyve","Yeşil","Hıyar","Tuz"
+                }
+            },
+            new WordCard
+            {
+                ID = 1,
+                Word = "ASD",
+                ForbiddenWords = new List<string>
+                {
+                    "ASD 1","ASD 2","ASD 3","ASD 4","ASD 5"
+                }
+            }
+        };
+
+        public static WordCard GetRandomWordCard()
+        {
+            Random random = new Random();
+            return WordCards[random.Next(0, WordCards.Count)];
+        }
+    }
+}

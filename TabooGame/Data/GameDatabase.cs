a8a920d8d8ca5fbdfa9ed1322fccecf264ad65ea
ugurdnis <ugurdnis@gmail.com>
2021-01-17 13:31:36 +0300

    Update GameDatabase.cs

--- TabooGame/Data/GameDatabase.cs
+++ TabooGame/Data/GameDatabase.cs
@@ -1,36 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using TabooGame.Models;
 
 namespace TabooGame.Data
 {
     public static class GameDatabase
     {
-        private static List<Player> Players = new List<Player>();
-        private static Game Game = new Game();
-        public static GameManager GameManager = new GameManager(Game);
-
-        #region Player Methods
-        public static Player GetPlayer(string id)
-        {
-            return Players.Where(x => x.ID == id).FirstOrDefault();
-        }
-
-        public static List<Player> GetPlayers()
-        {
-            return Players;
-        }
-
-        public static void AddPlayer(Player player)
-        {
-            Players.Add(player);
-        }
-
-        public static void RemovePlayer(Player player)
-        {
-            Players.Remove(player);
-        }
-
-        #endregion
+        public static List<Lobby> Lobbies = new List<Lobby>();
     }
 }

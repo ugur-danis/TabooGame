143a62703c9f9cfc679deb1ab3c6e93514323efd
ugurdnis <ugurdnis@gmail.com>
2021-01-17 13:26:36 +0300

    Deleted not required properties

--- TabooGame/Models/Player.cs
+++ TabooGame/Models/Player.cs
@@ -1,14 +1,8 @@
-﻿using static TabooGame.Data.GameDatabase;
-
-namespace TabooGame.Models
+﻿namespace TabooGame.Models
 {
     public class Player
     {
         public string ID { get; set; }
         public string Name { get; set; }
-        public bool IsAdmin { get; set; }
-        public Teams Team { get; set; }
-        public bool IsLobbyReady { get; set; }
-        public bool IsNextRoundReady { get; set; }
     }
 }

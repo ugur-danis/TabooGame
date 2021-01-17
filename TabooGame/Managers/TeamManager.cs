65670e2d14c35cabfed8d36610bf860cacf770ea
ugurdnis <ugurdnis@gmail.com>
2021-01-17 13:30:49 +0300

    Create TeamManager.cs

---
+++ TabooGame/Managers/TeamManager.cs
@@ -0,0 +1,20 @@
+﻿using TabooGame.Models;
+
+namespace TabooGame.Managers
+{
+    public static class TeamManager
+    {
+        public static void AddPlayer(this Team team, Player player)
+        {
+            if (!team.Players.Contains(player))
+                team.Players.Add(player);
+        }
+        public static void RemovePlayer(this Team team, Player player) =>
+            team.Players.Remove(player);
+        public static void ChangeTeam(this Player player, Team newTeam, Team oldTeam)
+        {
+            newTeam.AddPlayer(player);
+            oldTeam.RemovePlayer(player);
+        }
+    }
+}

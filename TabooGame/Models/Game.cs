dc357c599f5560af18d4b1ae7aa4464556cf8898
ugurdnis <ugurdnis@gmail.com>
2021-01-17 13:32:13 +0300

    Update Game.cs

--- TabooGame/Models/Game.cs
+++ TabooGame/Models/Game.cs
@@ -4,21 +4,33 @@ namespace TabooGame.Models
 {
     public class Game
     {
-        public Team Team1 { get; set; } = new Team() { Players = new List<Player>() };
-        public Team Team2 { get; set; } = new Team() { Players = new List<Player>() };
+        public Game(Lobby lobby, int numberOfWin, int counter, int rightToTaboo, int rightToPass)
+        {
+            Lobby = lobby;
+            NumberOfWin = numberOfWin;
+            Counter = counter;
+            RightToTaboo = rightToTaboo;
+            RightToPass = rightToPass;
+        }
+
+        public Lobby Lobby { get; set; }
+
         public int NumberOfWin { get; set; }
         public int Counter { get; set; }
         public int RightToTaboo { get; set; }
         public int RightToPass { get; set; }
-        public bool IsGameOver { get; set; }
-        public bool IsRoundStart { get; set; }
-        public Team CurrentPlayingTeam { get; set; } = new Team() { Players = new List<Player>() };
-        public Player CurrentSpeakerPlayer { get; set; }
-        public List<Player> CurrentListenerPlayers { get; set; } = new List<Player>();
-        public Teams CurrentOpponentPlayers { get; set; }
-        public Teams LastPlayedTeam { get; set; }
-        public List<Player> LastSpeakerPlayers { get; set; } = new List<Player>();
-        public List<WordCard> PastWordCards { get; set; } = new List<WordCard>();
+
+        public int TeamQueue { get; set; }
+        public int Team1PlayersQueue { get; set; }
+        public int Team2PlayersQueue { get; set; }
+
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+
+        public Team CurrentPlayingTeam { get; set; }
+        public Player CurrentNarratorPlayer { get; set; }
+
         public WordCard WordCard { get; set; }
+        public List<WordCard> PastWordCards { get; set; }
     }
 }

7a4701642d91c2919bf7483b9b8b42afef1731d0
Uğur Danış <41830280+ugurdnis@users.noreply.github.com>
2021-01-29 01:22:07 +0300

    Fix RoundStart and StartGame event

--- TabooGame/Hubs/GameHub.cs
+++ TabooGame/Hubs/GameHub.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using TabooGame.Data;
+using TabooGame.Managers;
 
 namespace TabooGame.Hubs
 {
     public class GameHub : Hub
     {
         public const string url = "/gameHub";
+        private static IHubCallerClients _clients;
+        private static GameHub _self;
+        private MyTimer _timer;
 
-        public override async Task OnConnectedAsync() =>
+        public override async Task OnConnectedAsync()
+        {
+            _clients = Clients;
+            _self = this;
             await Clients.Caller.SendAsync("GetPlayerID", Context.ConnectionId);
+        }
 
         #region LOGIN LOBBY
         public async Task CreateLobby(string adminID, string lobbyName) => await Groups.AddToGroupAsync(adminID, lobbyName);
@@ -21,11 +30,28 @@ namespace TabooGame.Hubs
 
         #region LOBBY
         public async Task ChangeTeam(string lobbyID) => await Clients.OthersInGroup(lobbyID).SendAsync("Refresh");
-        public async Task RoundStart(string lobbyID) => await Clients.Group(lobbyID).SendAsync("RoundStart");
+        #endregion
+
+        #region Round Start
+        public async Task RoundStart(string lobbyID)
+        {
+            GameDatabase.Lobbies.Find(x => x.ID == lobbyID).Game.GenerateGame();
+            await _clients.Group(lobbyID).SendAsync("RoundStart");
+            StartRoundStartCounter(lobbyID);
+        }
+        private void StartRoundStartCounter(string lobbyID)
+        {
+            _timer = new MyTimer(1000, GameConfig.StartRoundStartCounter, null, () => StartGameCounter(lobbyID));
+            _timer.StartTimer();
+        }
         #endregion
 
         #region GAME
-        public async Task StartGame(string lobbyID) => await Clients.OthersInGroup(lobbyID).SendAsync("StartGame");
+        private void StartGameCounter(string lobbyId, int counter = 5)
+        {
+            _timer = new MyTimer(1000, counter, null, async () => await _self.RoundStart(lobbyId));
+            _timer.StartTimer();
+        }
         #endregion
     }
 }

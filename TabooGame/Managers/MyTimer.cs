3bf6ce6acf0676de972762bdde85df3950738a31
Uğur Danış <41830280+ugurdnis@users.noreply.github.com>
2021-01-29 01:19:48 +0300

    Create MyTimer.cs

---
+++ TabooGame/Managers/MyTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace TabooGame.Managers
+{
+    public class MyTimer
+    {
+        private readonly Action _timeAction;
+        private readonly Action _timeEndAction;
+        private readonly Timer _timer;
+        public int Counter { get; private set; }
+
+        public MyTimer(double interval, int counter, Action timeAction = null, Action timeEndAction = null)
+        {
+            _timer = new Timer(interval);
+            Counter = counter;
+            _timeAction = timeAction;
+            _timeEndAction = timeEndAction;
+            _timer.Elapsed += (sender, e) => TimerElapsed(sender, e);
+        }
+        public void StartTimer()
+        {
+            _timer.Enabled = true;
+            _timer.Start();
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Counter <= 1)
+            {
+                _timer.Dispose();
+                _timeEndAction?.Invoke();
+            }
+
+            Counter--;
+            _timeAction?.Invoke();
+        }
+    }
+}

9c713fd0de4e94aa66b7bfe2a9a91687b05410f2
Uğur Danış <41830280+ugurdnis@users.noreply.github.com>
2021-01-30 15:29:26 +0300

    Rename StartRoundStartCounter to RoundStartCounter

--- TabooGame/Data/GameDatabase.cs
+++ TabooGame/Data/GameDatabase.cs
@@ -10,6 +10,7 @@ namespace TabooGame.Data
 
     public static class GameConfig
     {
+<<<<<<< HEAD
         public static int[] SelectNumberOfWin { get; } = new int[] { 10, 20, 30, 40, 50 };
         public static int[] SelectCounter { get; } = new int[] { 30, 40, 50, 60, 70, 80, 90 };
         public static int[] SelectRightToPass { get; } = new int[] { 1, 2, 3 };
@@ -17,5 +18,8 @@ namespace TabooGame.Data
         public static int DefaultNumberOfWinIndex { get; } = 0;
         public static int DefaultCounterIndex { get; } = 3;
         public static int DefaultRightToPassIndex { get; } = 2;
+=======
+        public static int StartRoundStartCounter { get; } = 5;
+>>>>>>> parent of 075ccec... Rename StartRoundStartCounter to RoundStartCounter
     }
 }

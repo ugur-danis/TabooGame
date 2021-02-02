436a7b937d35431c09e6e7827437d4da13b344f6
Uğur Danış <41830280+ugurdnis@users.noreply.github.com>
2021-02-02 23:36:41 +0300

    Create WordCardManager.cs

---
+++ TabooGame/Managers/WordCardManager.cs
@@ -0,0 +1,16 @@
+﻿using TabooGame.Data;
+using TabooGame.Models;
+
+namespace TabooGame.Managers
+{
+    public static class WordCardManager
+    {
+        public static WordCard GetWordCard(int id) => GameDatabase.WordCards.Find(x => x.ID == id);
+
+        public static WordCard GetRandomWordCard()
+        {
+            int random = new System.Random().Next(0, GameConfig.WordCardsCount);
+            return GetWordCard(random);
+        }
+    }
+}
